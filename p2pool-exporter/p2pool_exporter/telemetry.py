b5fe1ee625148c674b6fa86ad7a9d39c03a07fb6
ForgottenBeast <fb@devnull>
2025-09-30 13:32:35 +0200

    add miner telemetry

--- p2pool-exporter/p2pool_exporter/telemetry.py
+++ p2pool-exporter/p2pool_exporter/telemetry.py
@@ -1,5 +1,7 @@
 from functools import lru_cache, partial
-from opentelemetry.metrics import get_meter
+import json
+import redis
+from opentelemetry.metrics import get_meter, CallbackOptions, Observation
 from opentelemetry.instrumentation.urllib import URLLibInstrumentor
 from opentelemetry.instrumentation.asyncio import AsyncioInstrumentor
 
@@ -46,6 +48,46 @@ URLLibInstrumentor().instrument(
 )
 AsyncioInstrumentor().instrument()
 
-def initialize_telemetry():
+redis_client = None
+
+def miner_info_callback(options: CallbackOptions, miners):
+    global redis_client
+    for miner in miners:
+        data = redis_client.get(f"miner:{miner}")
+        attrs = {"miner":miner}
+        parsed = {}
+        if data:
+            parsed = json.loads(data)
+
+        value = parsed.get("last_share_height") or 0
+        yield Observation(value, attributes = attrs | {"metric":"last_share_height"})
+
+        value = parsed.get("hashrate") or 0
+        yield Observation(value,attributes = attrs | {"metric":"hashrate"})
+
+def miner_rewards_callback(options: Callbackoptions, miners):
+    global redis_client
+    for miner in miners:
+        data = redis_client.get(f"miner:{miner}")
+        attrs = {"mineer":miner}
+        parsed = {}
+        if data:
+            parsed = json.loads(data)
+        total_blocks = parsed.get("total_blocks") or 0
+        yield Observation(total_blocks, attributes = attrs |{"metric":"total_blocks"})
+
+        payouts = parsed.get("payouts") or 0
+        yield Observation(payouts, attributes = attrs |{"metric":"payouts"})
+
+
+
+def initialize_telemetry(redis_host, redis_port, miners):
+    global redis_client
+    redis_client = redis.Redis(host=redis_host, port=redis_port, db=0, protocol=3)
     meter = get_meter("p2pool-exporter")
-    #meter;create_observable_counter
+    meter.create_observable_gauge(name = "p2pool_exporter_miner_performance",
+                                  callbacks = [partial(miner_info_callback,miners = miners)],
+                                  description = "miner information")
+    meter.create_observable_up_down_counter(name = "p2pool_exporter_miner_rewards",
+                                    callbacks = [partial(miner_rewards_callback, miners = miners)],
+                                    description = "miner rewards info")
